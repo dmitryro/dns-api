f216f36693705a30cf4d067bf531746909f66c10
root <root@u17749893.onlinehome-server.com>
2017-01-01 23:14:07 -0500

    latest read_api

--- read_api.py
+++ read_api.py
@@ -3,28 +3,66 @@ import sys
 import csv
 
 from nsone import NSONE
-from nsone.rest.errors import AuthException
 from twisted.internet import defer
-from twisted.internet import reactor
+from twisted.internet import reactor,task
+from twisted.internet.task import react
+
+def handleMainSuccess(*args):
+    print "Success. Shutting down"
+    reactor.stop()
+    return
+   
+def handleMainError(failure):
+    print(failure)
+    reactor.stop()
+    return
 
 def handleSuccess(*args):
     print "Success. Shutting down"
-    reactor.stop()
+    return
 
 def handleError(failure):
     print(failure)
-    reactor.stop()
+    return
 
 
 @defer.inlineCallbacks
+def read_record(key,row):
+    # Create a dictionary to store type of records and their attributes  
+    nsone = NSONE(apiKey=key)
+    services = {'A':'add_A','MX':'add_MX','TXT':'add_TXT','CNAME':'add_CNAME'}
+
+    try:
+        # Try creating a zone
+        zone = yield nsone.createZone(row['Zone'], nx_ttl=row['TTL'])
+        print("Created a new zone %s"%(row['Zone']))
+
+    except Exception, R:
+        # Dismiss  - zone exists - load one
+        zone = yield nsone.loadZone(row['Zone'])
+        print("Zone %s already exists - loading"%(row['Zone']))
+
+           # we get the attribute dynamically to make it compact
+    func = getattr(zone,services[row['Type']])
+
+    try:
+        # call the add_X based on selection from dictionary
+        func(row['Zone'],row['Data'])
+        print("Added %s record %s %s"%(row['Type'],row['Zone'],row['Data']))
+
+    except Exception, R:
+        # Dismiss existing - nothing done, move on
+        print("Dismissed %s record %s %s"%(row['Type'],row['Zone'],row['Data']))
+          
+    qps = yield zone.qps()
+    defer.returnValue(qps)
+
+
 def main(fname,key):
    # connect the nsone API
-   nsone = NSONE(apiKey=key)
 
-   # Create a dictionary to store type of records and their attributes
-   services = {'A':'add_A','MX':'add_MX','TXT':'add_TXT','CNAME':'add_CNAME'}
+   tasks  = []
 
- 
    with open(fname) as csvfile:
        # Read csv
        reader = csv.DictReader(csvfile)
@@ -32,36 +70,22 @@ def main(fname,key):
        for row in reader:
            # Just print the zone and record data    
            print(row['Name'], row['Zone'], row['Type'], row['TTL'], row['Data'])
-           
-           try:
-               # Try creating a zone 
-               zone = nsone.createZone(row['Zone'], nx_ttl=row['TTL'])
-               print("Created a new zone %s"%(row['Zone']))
-            
-           except Exception, R:
-               # Dismiss  - zone exists - load one
-               zone = nsone.loadZone(row['Zone'])
-               print("Zone %s already exists - loading"%(row['Zone']))
-           
-           # we get the attribute dynamically to make it compact
-           func = getattr(zone,services[row['Type']])
-           
-           try:
-               # call the add_X based on selection from dictionary
-               func(row['Zone'],row['Data'])   
-               print("Added %s record %s %s"%(row['Type'],row['Zone'],row['Data']))
+           #d = task.deferLater(reactor, 0.05, read_record,key,row)
+           r = read_record(key,row)
+           tasks.append(r)
+           dl = defer.DeferredList(tasks)
 
-           except Exception, R:
-               # Dismiss existing - nothing done, move on
-               print("Dismissed %s record %s %s"%(row['Type'],row['Zone'],row['Data']))                 
+           d.addCallback(handleSuccess)
+           d.addErrback(handleError) 
 
- 
-
+     
 if __name__=='__main__':
    fname = sys.argv[1] 
    key = sys.argv[2] 
-   m = main(fname,key)
 
-   m.addCallback(handleSuccess)
-   m.addErrback(handleError)
+   d = task.deferLater(reactor, 0.5, main, fname, key)
+   d.addCallback(handleMainSuccess)
+   d.addErrback(handleMainError)
    reactor.run()
+
+
